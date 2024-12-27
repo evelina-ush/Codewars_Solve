19af4ed9aea011012ae6f2986624bbc8ac86b443
Evelina <eushakova300@gmail.com>
2024-12-27 13:47:56 +0300

    5 kyu kata solve

--- PaginationHelper_5kyu.py
+++ PaginationHelper_5kyu.py
@@ -31,7 +31,7 @@ class PaginationHelper:
             return remaining_items
 
         return self.items_per_page
-#always check this one
+#always check this one(had some problems with it)
     def page_index(self, item_index):
         if ((item_index / self.items_per_page) < 1) and (item_index >= 0) and (len(self.collection) > 0) and (
                 item_index < len(self.collection)):
